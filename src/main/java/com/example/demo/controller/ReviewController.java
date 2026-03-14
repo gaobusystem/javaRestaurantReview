214bb50bb507e67627b8ed346368cbf36179a3fa
yamato2323 <dev@gaobusystem.com>
2026-03-14 10:59:54 +0900

    10 Validation

--- src/main/java/com/example/demo/controller/ReviewController.java
+++ src/main/java/com/example/demo/controller/ReviewController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,14 +27,31 @@ public class ReviewController {
 
 	/*--- レビュー登録リクエスト（登録画面より） ---*/
 	@PostMapping("/regist-review")
-	public String registReview(@ModelAttribute ReviewRegistForm form) {
+	public String registReview(
+			@Validated @ModelAttribute ReviewRegistForm form,
+			BindingResult result) {
+
+		// 入力エラーがある場合には レビュー登録画面に戻す
+		if (result.hasErrors()) {
+			return "regist-review";
+		}
+		
+		// 正常な場合に レビュー登録確認画面に 遷移する
 		return "confirm-regist-review";
 	}
 
 	/*--- レビュー登録リクエスト（登録確認画面より） ---*/
 	@PostMapping("/confirm-regist-review")
-	public String confirmRegistReview(ReviewRegistForm form,
+	public String confirmRegistReview(
+			@Validated ReviewRegistForm form,
+			BindingResult result,
 			Model model) {
+
+		// 入力エラーがある場合には レビュー登録画面に戻す
+		if (result.hasErrors()) {
+			return "regist-review";
+		}
+
 		//
 		// *** ここで from 内容で DB登録処理を行う ***
 		//
