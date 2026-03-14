6b55abf522d0c440e561d8d530e3da6bec455a05
yamato2323 <dev@gaobusystem.com>
2026-03-14 10:57:25 +0900

    07 Postリクエスト

--- src/main/java/com/example/demo/controller/ParamController.java
+++ src/main/java/com/example/demo/controller/ParamController.java
@@ -3,18 +3,21 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ParamController {
 
-	@GetMapping("/get-param")
-	public String getParam(@RequestParam String greeting,
-//	public String getParam(@RequestParam(name="g") String greeting,
-//	public String getParam(@RequestParam(required=false) String greeting,
-//	public String getParam(@RequestParam(defaultValue="(未設定)") String greeting,
-			Model model) {
+	@GetMapping("/input")
+	public String input() {
+		return "input";
+	}
 	
+	@PostMapping("/post-param")
+	public String postParam(@RequestParam String greeting,
+			Model model) {
+
 		model.addAttribute("mGreeting", greeting);
 		
 		return "display";
