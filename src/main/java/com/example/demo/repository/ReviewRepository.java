2c1a71c886248b7d1905b6aad8b00217a8072a61
yamato2323 <dev@gaobusystem.com>
2026-03-14 19:28:49 +0900

    21 レビュー一覧DB検索

--- src/main/java/com/example/demo/repository/ReviewRepository.java
+++ src/main/java/com/example/demo/repository/ReviewRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import com.example.demo.entity.Review;
 
 public interface ReviewRepository {
 
 	void add(Review review);
+	
+	List<Review> selectByRestaurantId(int restaurantId);
 	
 }
