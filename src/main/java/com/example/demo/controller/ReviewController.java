9baba4c93e2145d0b661d5cd265fdc02acdd202c
yamato2323 <dev@gaobusystem.com>
2026-03-14 10:58:50 +0900

    09 form初期化

--- src/main/java/com/example/demo/controller/ReviewController.java
+++ src/main/java/com/example/demo/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,15 +13,33 @@ public class ReviewController {
 
 	/*--- レビュー登録画面表示リクエスト ---*/
 	@GetMapping("/show-review-form")
-	public String showReviewForm() {
+	public String showReviewForm(@ModelAttribute ReviewRegistForm form) {
+		return "regist-review";
+	}
+
+	/*--- レビュー登録画面表示リクエスト（確認画面からの戻り） ---*/
+	@PostMapping("/show-review-form-ret")
+	public String showReviewFormRet(@ModelAttribute ReviewRegistForm form) {
 		return "regist-review";
 	}
 
 	/*--- レビュー登録リクエスト（登録画面より） ---*/
 	@PostMapping("/regist-review")
 	public String registReview(@ModelAttribute ReviewRegistForm form) {
-		System.out.println(form);		// 内容をチェック
 		return "confirm-regist-review";
+	}
+
+	/*--- レビュー登録リクエスト（登録確認画面より） ---*/
+	@PostMapping("/confirm-regist-review")
+	public String confirmRegistReview(ReviewRegistForm form,
+			Model model) {
+		//
+		// *** ここで from 内容で DB登録処理を行う ***
+		//
+
+		model.addAttribute("msg", "レビュー登録が完了しました。");
+		
+		return "complete-regist-review";
 	}
 
 }
